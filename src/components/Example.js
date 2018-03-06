636a9874203d89d8cd41ac12053540ce5fe8737d
Cheng Xiancheng <chengxc1990@outlook.com>
2018-03-06 16:01:08 +0800

    dva-cli配置

---
+++ src/components/Example.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Example = () => {
+  return (
+    <div>
+      <div />
+      <div />
+      <div />
+
+    </div>
+  );
+};
+
+Example.propTypes = {
+};
+
+export default Example;
